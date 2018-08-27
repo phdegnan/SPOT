f5df5b9e9c3263cd5fb95b67281d84df3b959333
Patrick Degnan <16374715+phdegnan@users.noreply.github.com>
2018-08-27 15:14:53 -0700

    Update rerun_enrichment.pl

--- IntaRNA1.0.4_modified_scripts/rerun_enrichment.pl
+++ IntaRNA1.0.4_modified_scripts/rerun_enrichment.pl
@@ -4,7 +4,7 @@ unless(@ARGV){die "\nUsage: $0 intarna_websrv_table_ncbi.csv \n\n";}
 
 #$ARGV[0]
 
-my $PATH_HOME="/data/software/IntaRNA1.0.4";
+my $PATH_HOME="/software/IntaRNA1.0.4";
 my $PATH_R="/usr/bin";
 
 system "$PATH_HOME/termClusterReport.pl $ARGV[0]";
@@ -13,7 +13,7 @@ system "cp $PATH_HOME/intarna_heatmap.html ."; ## edit 1.0.2 ## edit 1.0.3 bugfi
 system "$PATH_HOME/make_heatmap_json.pl enrichment.txt"; ## edit 1.0.2
 system "cp $PATH_HOME/index-thumb.html ."; ## edit 1.0.2
 system "cp $PATH_HOME/index-pdf.html ."; ## edit 1.0.3
-system "/usr/local/bin/phantomjs $PATH_HOME/rasterize.js ./index-thumb.html enriched_heatmap_big.png";
-system "/usr/local/bin/phantomjs $PATH_HOME/rasterize.js ./index-pdf.html enriched_heatmap_big.pdf"; ## edit 1.0.3
+system "$PATH_HOME/phantomjs $PATH_HOME/rasterize.js ./index-thumb.html enriched_heatmap_big.png";
+system "$PATH_HOME/phantomjs $PATH_HOME/rasterize.js ./index-pdf.html enriched_heatmap_big.pdf"; ## edit 1.0.3
 system "rm index-thumb.html"; ## edit 1.0.2
 system "rm index-pdf.html"; ## edit 1.0.3
